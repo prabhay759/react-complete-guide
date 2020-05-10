8682046aaef1ab7b23f1c18f212ea4ac2d8789cc
Prabhay Gupta <prabhay.gupta@ef.com>
2020-05-10 16:22:31 +0200

    Delete handled to delete the things

--- src/App.js
+++ src/App.js
@@ -1,98 +1,140 @@
-import React, { useState } from 'react';
-import './App.css';
-import Person, {person1 as Person1} from './Person/Person';
-import UserInput from './UserInput/UserInput'
-import UserOutput from './UserOutput/UserOutput'
+import React, { useState, Component } from "react";
+import "./App.css";
+import Person, { person1 as Person1 } from "./Person/Person";
 
-const app = props => {
-  const [personsState, setPersonState] = useState({
+// Assignment purpose
+// import UserInput from './UserInput/UserInput'
+// import UserOutput from './UserOutput/UserOutput'
+
+// Functional way of setting state.
+
+// const [personsState, setPersonState] = useState({
+//   persons: [
+//     {
+//       name: "Max",
+//       age: 28,
+//     },
+//     {
+//       name: "Menu",
+//       age: 29,
+//     },
+//     {
+//       name: "Stephanie",
+//       age: 28,
+//     },
+//   ],
+//   otherState: "Some other value",
+// });
+
+class app extends Component {
+  state = {
     persons: [
       {
-        name: 'Max',
-        age: 28
+        name: "Max",
+        age: 28,
       },
       {
-        name: 'Menu',
-        age: 29
+        name: "Menu",
+        age: 29,
       },
       {
-        name: 'Stephanie',
-        age: 28
-      }
+        name: "Stephanie",
+        age: 28,
+      },
     ],
-    otherState: 'Some other value',
-  })
+    otherState: "Some other value",
+    showPersons: false,
+  };
 
+  // switchNamehandler = (newName) => {
+  //   this.setState({
+  //     persons: [
+  //       {
+  //         name: newName,
+  //         age: 28,
+  //       },
+  //       {
+  //         name: "Menu",
+  //         age: 29,
+  //       },
+  //       {
+  //         name: "Stephanie",
+  //         age: 27,
+  //       },
+  //     ],
+  //   });
+  // };
 
+  deletePersonHandler = (index) => {
+    const persons = this.state.persons;
+    persons.splice(index, 1);
+    this.setState({ persons });
+  };
 
-  const switchNamehandler = (newName) => {
-    setPersonState({
+  nameChangedHandler = (event) => {
+    this.setState = {
       persons: [
         {
-          name: newName,
-          age: 28
-        },
-        {
-          name: 'Menu',
-          age: 29
-        },
-        {
-          name: 'Stephanie',
-          age: 27
-        }
-      ],
-      otherState: personsState.otherState,
-    })
-  }
-
-  const nameChangedHandler = (event) => {
-    setPersonState({
-      persons: [
-        {
-          name: 'Maxmillian',
-          age: 28
+          name: "Maxmillian",
+          age: 28,
         },
         {
           name: event.target.value,
-          age: 29
+          age: 29,
         },
         {
-          name: 'Stephanie',
-          age: 27
-        }
+          name: "Stephanie",
+          age: 27,
+        },
       ],
-      otherState: personsState.otherState,
-    })
-  }
-
-  // Inline style.
-  // styling the button
-  // styling can be applied globally.
-  const style = {
-    backgroundColor: 'green',
-    font: 'inherit',
-    border: '1px solid blue',
-    padding: '8px',
-    cursor: 'pointer'
+    };
   };
 
-  return (
-    <div className="App">
-      <h1>I am a React developer</h1>
-      <p>This is really working!</p>
-      <button 
-      onClick={switchNamehandler.bind(this, 'Prabhay a new name')} style={style}>Switch Name
-      </button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}></Person>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}
-        click={switchNamehandler.bind(this, 'Prabhay!!')} 
-        changed={nameChangedHandler}>
-      </Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>My hobbies are playing chess and Carrom!!</Person>
-      {/* <Person1 age='Person1 component from person class' name={personsState.persons[2].name}></Person1> */}
-    </div>
-  );
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
+  };
+
+  render() {
+    // Inline style.
+    // styling the button
+    // styling can be applied globally.
+    const style = {
+      backgroundColor: "green",
+      font: "inherit",
+      border: "1px solid blue",
+      padding: "8px",
+      cursor: "pointer",
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+                name={person.name}
+                age={person.age}
+                click={this.deletePersonHandler.bind(this, index)}
+              ></Person>
+            );
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <p>This is really working!</p>
+        <button onClick={this.togglePersonHandler} style={style}>
+          Toggle Person
+        </button>
+        {persons}
+      </div>
+    );
+  }
 }
 
 export default app;
-  
